eb54555eeea70840dc2c3aaa642cf330832fb8eb
Stéphane Este-Gracias <sestegra@gmail.com>
2021-06-30 17:05:00 +0200

    release: 2.0.8
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

--- Formula/sac.rb
+++ Formula/sac.rb
@@ -2,9 +2,9 @@ class Sac < Formula
   desc "Slides as Code"
   homepage "https://github.com/sacproj"
 
-  url "https://github.com/sacproj/sac-cli/releases/download/2.0.7/sac-cli.tar.gz"
-  version "2.0.7"
-  sha256 "977c8c9db887de8d01b6e8ff529ca74c218321f4c1b20af23273b7482d3031b6"
+  url "https://github.com/sacproj/sac-cli/releases/download/2.0.8/sac-cli.tar.gz"
+  version "2.0.8"
+  sha256 "1254b430919587440cd4030ba3eead8acb298d36efe7602a8e826e1538703086"
   license "MIT"
 
   def install
